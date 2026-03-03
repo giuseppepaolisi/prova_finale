b84905d883aeeb153801b69b925a3b740f82e427
Rayquasi <45846780+giuseppepaolisi@users.noreply.github.com>
2026-03-03 19:02:04 +0100

    Update subscript_model.py

--- backend/src/models/subscript_model.py
+++ backend/src/models/subscript_model.py
@@ -8,6 +8,11 @@ class SubscriptModel:
     def join(user, event, conn):
         try:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                # BLOCCO della riga dell'evento prima di fare qualsiasi altra cosa
+                # Se un'altra transazione sta già lavorando su questo evento, 
+                # il codice si fermerà qui finché l'altra non ha finito.
+                query_lock = "SELECT id_event FROM events WHERE id_event = %s FOR UPDATE"
+                cur.execute(query_lock, (event,))
                 query_insert = "INSERT INTO subscriptions (id_user, id_event) VALUES (%s, %s) RETURNING *"
                 
                 cur.execute(query_insert, (user, event))
@@ -18,6 +23,10 @@ class SubscriptModel:
                 if subscript is None:
                     raise HTTPException(status_code=404, detail="Non è possibile iscriversi all'evento")
                 
+                query_update_events = "UPDATE events SET subscriptions = subscriptions + 1 WHERE id_event = %s RETURNING *"
+                cur.execute(query_update_events, (event,))
+                event_data = cur.fetchone()
+            
                 return subscript
                 
         except psycopg2_errors.UniqueViolation:
